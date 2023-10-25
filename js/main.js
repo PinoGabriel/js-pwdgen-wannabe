b8cdfee60b8032fbafc5bf00bf82456c37a394c8
PinoGabriel <pinogabriel01@gmail.com>
2023-10-25 15:34:00 +0200

    first commit

---
+++ js/main.js
@@ -0,0 +1,10 @@
+
+
+const name = prompt ("Scrivi il tuo nome", "Gabriel")
+
+const surname = prompt ("Scrivi il tuo cognome", "Pino")
+
+const color = prompt ("Scrivi il tuo colore preferito", "Blu")
+
+
+document.getElementById("password").innerHTML = "La tua password è: " + name + surname + color + "#23";
